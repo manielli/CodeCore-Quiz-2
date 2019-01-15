40a875c65aa18341478fe3d5c7cd0be53b31e83f
Mani Jafari <manielli@gmail.com>
2019-01-15 11:43:51 -0800

    minor visual improvements

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,14 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed")
-});
-
-for(var i = 0; i < 1000000000; i++){
-
+function doSomething() {
+    console.info("DOM Loaded");
 }
+
+if (document.readyState === "Loading...") {
+    document.addEventListener("DOMContentLoaded", doSomething) 
+} else
+{
+    doSomething();
+};
 
 document.querySelectorAll(".card-title").forEach( node => {
     node.addEventListener( "click", event => {
-        console.log(`${event.currentTarget.id} was clicked!`);
+        // console.log(`${event.currentTarget.id} was clicked!`);
         const ideaNode = event.currentTarget;
+        console.log(ideaNode.id);
     });
 });
